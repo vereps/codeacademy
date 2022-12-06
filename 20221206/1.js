1b677c9e7712ca980d563324272bf0df7d0544ce
vereps <vereps@gmail.com>
2022-12-06 20:24:11 +0200

    2darbas

--- 20221206/1.js
+++ 20221206/1.js
@@ -6,8 +6,8 @@
 // Atspausdins consolėje "trio" jei muzikantų yra 3;
 // Atspausdins consolėje "kvartetas" jei muzikantų yra 4;
 // Atspausdins consolėje "didelė grupė" jei muzikantų yra daugiau nei 4.
-let music = [0, 1, 2, 3, 4, 5];
-console.log(music);
+// let music = [0, 1, 2, 3, 4, 5];
+// console.log(music);
 // function audio(music) {
 //     let result;
 //     if (music > 0) {
@@ -16,18 +16,28 @@ console.log(music);
 //     return result;
 //     console.log(result);
 // }
-let filteredMusic0 = music.filter(num => num <= 0);
-console.log(`ne grupė`);
-let filteredMusic1 = music.filter(num => num == 1);
-console.log(`solo`);
-let filteredMusic2 = music.filter(num => num == 2);
-console.log(`duetas`);
-let filteredMusic3 = music.filter(num => num == 3);
-console.log(`trio`);
-let filteredMusic4 = music.filter(num => num == 4);
-console.log(`kvartetas`);
-let filteredMusic5 = music.filter(num => num > 4);
-console.log(`didelė grupė`);
-
-
-
+// let filteredMusic0 = music.filter(num => num <= 0);
+// console.log(`ne grupė`);
+// let filteredMusic1 = music.filter(num => num == 1);
+// console.log(`solo`);
+// let filteredMusic2 = music.filter(num => num == 2);
+// console.log(`duetas`);
+// let filteredMusic3 = music.filter(num => num == 3);
+// console.log(`trio`);
+// let filteredMusic4 = music.filter(num => num == 4);
+// console.log(`kvartetas`);
+// let filteredMusic5 = music.filter(num => num > 4);
+// console.log(`didelė grupė`);
+// --------------------------------------------------
+// Funkcija grąžins true, jei parametro amžius yra didesnis nei 18 metų.
+//       function checkAge(age) {​
+//   if (age > 18) {​
+//     return true;
+//   }​ else {​
+//     return confirm('Did parents allow you?');
+//   }​
+// }​
+// Perrašykite funkcijos kodą, kad jis būtų be if ir vienoje eilutėje:
+// Naudokite: ternary operator ?
+function checkAge(age) {return age > 18 ? true : false;};
+console.log(checkAge(43));
