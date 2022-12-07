d12facc6f29f294a0073b1f99f5584b5904849df
vereps <vereps@gmail.com>
2022-12-07 15:38:03 +0200

    20221206_task

--- 20221206/1.js
+++ 20221206/1.js
@@ -1,3 +1,4 @@
+console.log(`-------task1-----------`);
 // Muzikinės grupės turi specialius pavadinimus pagal žmonių skaičių grupėje. Pvz., „kvartetas“ yra muzikinė grupė, kurią sudaro keturi muzikantai.
 // Parašykite sąlyginių teiginių seriją (else...if statements), kuri:
 // Atspausdins consolėje "ne grupė" jei muzikantų yra mažiau ar lygu 0;
@@ -6,29 +7,24 @@
 // Atspausdins consolėje "trio" jei muzikantų yra 3;
 // Atspausdins consolėje "kvartetas" jei muzikantų yra 4;
 // Atspausdins consolėje "didelė grupė" jei muzikantų yra daugiau nei 4.
-// let music = [0, 1, 2, 3, 4, 5];
-// console.log(music);
-// function audio(music) {
-//     let result;
-//     if (music > 0) {
-//         result = `ne grupė" jei muzikantų yra mažiau ar lygu 0`;
-//     } else {result = `ELSE`;}
-//     return result;
-//     console.log(result);
-// }
-// let filteredMusic0 = music.filter(num => num <= 0);
-// console.log(`ne grupė`);
-// let filteredMusic1 = music.filter(num => num == 1);
-// console.log(`solo`);
-// let filteredMusic2 = music.filter(num => num == 2);
-// console.log(`duetas`);
-// let filteredMusic3 = music.filter(num => num == 3);
-// console.log(`trio`);
-// let filteredMusic4 = music.filter(num => num == 4);
-// console.log(`kvartetas`);
-// let filteredMusic5 = music.filter(num => num > 4);
-// console.log(`didelė grupė`);
-// --------------------------------------------------
+let num = ('');
+let result = 'ne grupė';
+if (num == 1) {
+    result = `solo`;
+} else if (num == 2) {
+    result = `duetas`;
+} else if (num == 3) {
+    result = `trio`;
+} else if (num == 4) {
+    result = `kvartetas`;
+} else if (num > 4) {
+    result = `didelė grupė`;
+} else {
+    result = `ne grupė`;
+}
+console.log(result);
+// ---------------------------------------
+console.log(`-------task2-----------`);
 // Funkcija grąžins true, jei parametro amžius yra didesnis nei 18 metų.
 //       function checkAge(age) {​
 //   if (age > 18) {​
@@ -39,14 +35,28 @@
 // }​
 // Perrašykite funkcijos kodą, kad jis būtų be if ir vienoje eilutėje:
 // Naudokite: ternary operator ?
-function checkAge(age) {return age > 18 ? true : `Did parents allow you?`;};
+function checkAge(age) { return age > 18 ? true : `Did parents allow you?`; };
 console.log(checkAge(43));
-// -------3-----------
+// ---------------------------------------
+console.log(`-------task3-----------`);
 // Parašykite „JavaScript“ funkciją, kuri grąžina perduotą eilutę su raidėmis abėcėlės tvarka.
 console.assert(alphabetOrder('alphabetical') == 'aaabcehillpt');
-
-function alphabetOrder(word){
-    return word.split("").sort().join("").toLowerCase();
+function alphabetOrder(word) {
+    return [...word].sort().join("").toLowerCase();
 }
 console.log(alphabetOrder(`Naujas miestas`));
-// console.assert(alphabetOrder('alphabetical') == 'aaabcehillpt');
+// ---------------------------------------
+console.log(`-------task4-----------`);
+// [21:51] Kirilas Čalkin"plusPlus" Function [JavaScript]
+// Parašykitė funkciją plusplus kuri priima 2 parametrus:
+//  skaičius	
+//  kiek kartu prie šio skaičiaus reikia pridėti 1
+// Pvz.: plusPlus(10, 3) grąžina 13. Svarbu: turi pridėti butent 1, o ne tesiog sudėti du skaičius
+function plusPlus(num, x) {
+    for (let i = 0; i < x; i++) {
+        num++;
+    }
+    return num;
+}
+console.log(plusPlus(10, 3));
+// ---------------------------------------
