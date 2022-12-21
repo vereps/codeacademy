7c071a403b3dba1cc58ebd8677262804a2f63ae4
vereps <vereps@gmail.com>
2022-12-21 20:54:14 +0200

    CPU

---
+++ script.js
@@ -0,0 +1,6 @@
+// Naudodami NodeJS parodykite procesoriaus pavadinimą ir jame esančių branduolių skaičių
+const os = require('os');
+const systemCpuCores = os.cpus();
+const cpuName = os.cpus()[0].model;
+const numOfCpus = os.cpus().length
+console.log(`${cpuName} Branduoliu: ${numOfCpus}`);
