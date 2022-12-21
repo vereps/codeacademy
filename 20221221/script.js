030227062bfb2e9126ca9e7ffd00938814e04a98
vereps <vereps@gmail.com>
2022-12-21 20:48:46 +0200

    CPU

--- 20221221/script.js
+++ 20221221/script.js
@@ -1,1 +1,6 @@
-console.log('veikia');
+// Naudodami NodeJS parodykite procesoriaus pavadinimą ir jame esančių branduolių skaičių
+const os = require('os');
+const systemCpuCores = os.cpus();
+const cpuName = os.cpus()[0].model;
+const numOfCpus = os.cpus().length
+console.log(`${cpuName} Branduoliu: ${numOfCpus}`);
