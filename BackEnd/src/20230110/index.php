79d8d918e11b2810f0c0ad4f377e747b7c347380
vereps <vereps@gmail.com>
2023-01-10 20:10:55 +0200

    OS

--- BackEnd/src/20230110/index.php
+++ BackEnd/src/20230110/index.php
@@ -1,5 +1,11 @@
 <?php
 // Naudojant PHP išveskite į naršyklę: "My name is {vardas}. My age is {amžius}"
-$name = 'Remigijus';
-$age = 43;
-echo "My name is {$name}. My age is {$age}";
+// $name = 'Remigijus';
+// $age = 43;
+// echo "My name is {$name}. My age is {$age}";
+
+// Panaudojus "Execution operator" parodykite opėracinės sistemos informaciją, kurioje veikia PHP
+// echo PHP_OS;
+
+$output = PHP_OS;
+echo "<pre>$output</pre>";
