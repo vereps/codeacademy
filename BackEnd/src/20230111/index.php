4d84af8549c1cf0c3440edfc462028b3932d9c81
vereps <vereps@gmail.com>
2023-01-11 19:34:48 +0200

    weekend

--- BackEnd/src/20230111/index.php
+++ BackEnd/src/20230111/index.php
@@ -3,10 +3,10 @@
 // Sukurkite dvimatį masyvą. Pirmieji du raktai yra lt ir en.
 // Raktai turi savaitės dienų vardų masyvus lietuviškai ir angliškai.
 define('NEW_LINE', "<br>\n");
-$weekDay = array(
+$weekDay = [
     'lt' => ['Pirmadinis', 'Antradienis', 'Trečiadinis', 'Ketvirtadienis', 'Penktadienis', 'Šeštadienis', 'Sekmadinis'],
     'en' => ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-);
+];
 // Naudodamiesi šiuo masyvu, pirmadienį parodykite lietuvių kalba, o trečiadienį - anglų kalba.
 echo "{$weekDay['lt'][0]} {$weekDay['en'][2]}" . NEW_LINE;
 // Sukurkite kintamuosius lang (reikšmės lt arba en) ir parodykite dieną
